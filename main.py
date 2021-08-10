8a11de774f89f0ce0900505093e4794ff28e2c5f
Aleksandr <6450625@gmail.com>
2021-08-10 21:24:12 +0700

    Addet Tabs Navigation

--- main.py
+++ main.py
@@ -5,6 +5,12 @@ from kivy.properties import StringProperty, ListProperty
 from kivymd.app import MDApp
 from kivymd.theming import ThemableBehavior
 from kivymd.uix.list import OneLineIconListItem, MDList
+from kivymd.uix.tab import MDTabsBase
+from kivymd.uix.floatlayout import MDFloatLayout
+from kivymd.icon_definitions import md_icons
+from kivymd.font_definitions import fonts
+
+
 
 KV = '''
 # Menu item in the DrawerList list.
@@ -70,8 +76,8 @@ Screen:
                         elevation: 10
                         left_action_items: [['menu', lambda x: nav_drawer.set_state("open")]]
 
-                    Widget:
-                     
+                    MDTabs:
+                        id: tabs
 
 
         MDNavigationDrawer:
@@ -85,6 +91,8 @@ Screen:
 class ContentNavigationDrawer(BoxLayout):
     pass
 
+class Tab(MDFloatLayout, MDTabsBase):
+    pass
 
 class ItemDrawer(OneLineIconListItem):
     icon = StringProperty()
@@ -120,6 +128,15 @@ class TehOsmotrApp(MDApp):
             self.root.ids.content_drawer.ids.md_list.add_widget(
                 ItemDrawer(icon=icon_name, text=icons_item[icon_name])
             )
+
+        #for name_tab in list(md_icons.keys())[15:30]:
+        #    self.root.ids.tabs.add_widget(Tab(icon=name_tab, title=name_tab))
+
+        for icon_name, name_tab in icons_item.items():
+            self.root.ids.tabs.add_widget(
+                Tab(text=f" [ref={name_tab}][font={fonts[-1]['fn_regular']}]{md_icons[icon_name]}[/font][/ref] {name_tab}")
+            )
+
 
 
 TehOsmotrApp().run()
